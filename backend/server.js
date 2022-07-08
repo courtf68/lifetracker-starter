bb59254461373cf1f83b94f8714f1a2fbeb02786
Courtney Fenderson <cfenderson@cfender-ltmv2wd.internal.salesforce.com>
2022-07-08 12:00:33 -0700

    backend kinda

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,20 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const morgan = require("morgan");
-const cors = require("cors");
+const cors = require("cors"); //fixes posty issues
+const auth = require("./routes/auth");
+
+app.use(cors()); //remember func
+app.use(morgan("tiny"));
+app.use(express.json());
+app.use("auth", auth);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+app.post("/", (req, res) => {
+  console.log(req.body); //need another post thing w str?
+  res.send("good");
 });
 
 app.listen(port, () => {
