8f72d93f791cf67e2856a66fc135e361a519d004
Courtney Fenderson <cfenderson@cfender-ltmv2wd.internal.salesforce.com>
2022-07-14 10:07:43 -0700

    inclass

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,28 @@
 const express = require("express");
 const router = express.Router();
 
+const User = require("../models/users");
+
+router.post("/login", async (req, res, next) => {
+  try {
+    const user = await User.login(req.body);
+
+    return res.status(200).json({ user });
+  } catch (err) {
+    next(err);
+  }
+});
+router.post("/register", async (req, res, next) => {
+  try {
+    const user = await User.register(req.body);
+    // const token = createUserJwt(user);
+    return res.status(201).json({ user });
+  } catch (err) {
+    next(err);
+  }
+});
+
+//ex below
 // middleware that is specific to this router
 router.use((req, res, next) => {
   console.log("Time: ", Date.now());
